5290267db806ca3c58654dd3aa03cae6b34f93a4
ofurkanmeral <fom1329@gmail.com>
2022-04-25 14:11:54 +0300

    base and product controller

---
+++ WebApi/Controllers/CustomBaseController.cs
@@ -0,0 +1,31 @@
+﻿using Core.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.statusCode == 204)
+            {
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.statusCode
+                };
+            }
+            return new ObjectResult(response)
+            {
+                StatusCode = response.statusCode
+            };
+        }
+    }
+}
---
+++ WebApi/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Core;
+using Core.DTOs;
+using Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+
+    public class ProductController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IServices<Product> _service;
+        public ProductController(IMapper mapper, IServices<Product> service)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _service.GetAll();
+            var productDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productDtos));
+        }
+        [HttpGet("id")]
+        public async Task<IActionResult>GetById(int id)
+        {
+            var product = await _service.GetById(id);
+            var productDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productDto));
+        }
+        
+    }
+}
